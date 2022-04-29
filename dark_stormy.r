2b1c1c2873de4d3706c1584c907cd6bb48ff973c
jstone-23 <101530181+jstone-23@users.noreply.github.com>
2022-04-29 15:22:03 -0400

    Update dark_stormy.r

--- dark_stormy.r
+++ dark_stormy.r
@@ -1,1 +1,1 @@
-"It was a dark and stormy night so I stayed in to finish my R project. (Helped me avoid COVID-19 too.")
+"It was a dark and stormy night so I stayed in to finish my R project. (Helped me avoid COVID-19 too.)"
